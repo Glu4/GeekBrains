7b32ccbedc9851240eec65005fdc480f00223ca0
Glu4 <avozhak@gmail.com>
2023-07-09 17:50:11 +0300

    Sucsses!

---
+++ Seminar2/HomeWork/Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿        //Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+        //645 -> 5
+        //78 -> третьей цифры нет
+        //32679 -> 6
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+        while(true)
+        {
+
+        
+            if (number < 99)
+    
+       
+
+        Console.WriteLine("В числе нет третьей цифры");
+        
+         
+        {
+            break;
+        }
+            
+        }
+
+
+        int thirdDigit = number % 10;
+
+        if (thirdDigit != 0)
+        {
+            Console.WriteLine($"Третья цифра числа: {thirdDigit}");
+            }
+
+        
