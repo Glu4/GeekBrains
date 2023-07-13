79ccb1f1b467d6f8ce251d1b4cea4356997b706a
Glu4 <avozhak@gmail.com>
2023-07-13 18:55:38 +0300

    Succes

--- Seminar3/Homework/Task2/Program.cs
+++ Seminar3/Homework/Task2/Program.cs
@@ -8,6 +8,16 @@ namespace Distance
 {
     class Program
     {
+        static double CalculateDistance(double x1, double y1, double z1, double x2, double y2, double z2)
+        {
+            double dx = x2 - x1;
+            double dy = y2 - y1;
+            double dz = z2 - z1;
+
+            double distance = Math.Sqrt(dx * dx + dy * dy + dz * dz);
+            return distance;
+        }
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Введите координаты первой точки:");
@@ -29,15 +39,7 @@ namespace Distance
             double distance = CalculateDistance(x1, y1, z1, x2, y2, z2);
             Console.WriteLine("Расстояние между точками: " + distance.ToString("F2"));
         }
-
-        static double CalculateDistance(double x1, double y1, double z1, double x2, double y2, double z2)
-        {
-            double dx = x2 - x1;
-            double dy = y2 - y1;
-            double dz = z2 - z1;
-
-            double distance = Math.Sqrt(dx * dx + dy * dy + dz * dz);
-            return distance;
-        }
+        
+        
     }
 }
