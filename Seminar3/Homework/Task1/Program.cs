77790801fcb08fdba863e2e3f05497a542d2d4c0
Glu4 <avozhak@gmail.com>
2023-07-13 17:38:53 +0300

    Succes

---
+++ Seminar3/Homework/Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static bool IsPalindrome(int number)
+    {
+        int temp = number;
+        int reversedNumber = 0;
+
+        while (temp != 0)
+        {
+            int remainder = temp % 10;
+            reversedNumber = reversedNumber * 10 + remainder;
+            temp /= 10;
+        }
+
+        return reversedNumber == number;
+    }
+
+    static void Main()
+    {
+        Console.Write("Введите пятизначное число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number < 10000 || number > 99999)
+        {
+            Console.WriteLine("Введено неверное число.");
+            return;
+        }
+
+        if (IsPalindrome(number))
+        {
+            Console.WriteLine("Да, число является палиндромом.");
+        }
+        else
+        {
+            Console.WriteLine("Нет, число не является палиндромом.");
+        }
+    }
+}
