5f6690616950308f69ed7758296ed0130c7dd2a4
Glu4 <avozhak@gmail.com>
2023-07-19 19:27:22 +0300

    success!

---
+++ Seminar4/Homework/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+class Program
+{
+    static int CalculatePower(int a, int b)
+    {
+        int result = 1;
+        for (int i = 0; i < b; i++)
+        {
+            result *= a;
+        }
+        return result;
+    }
+
+
+    static void Main()
+    {
+        Console.WriteLine("Введите число A:");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите число B:");
+        int b = int.Parse(Console.ReadLine());
+
+        int result = CalculatePower(a, b);
+        Console.WriteLine("Результат: " + result);
+    }
+
+    
+}
