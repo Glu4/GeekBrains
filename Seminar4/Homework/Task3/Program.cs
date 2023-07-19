389f8456ef7036fd5245896f7ddbb45be80e583a
Glu4 <avozhak@gmail.com>
2023-07-19 19:52:07 +0300

    success!

---
+++ Seminar4/Homework/Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+
+class Program
+{
+    static void Main()
+    {
+       
+        Random random = new Random();
+
+        
+        int[] arr = new int[8];
+
+        
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = random.Next(1, 100); 
+        }
+        
+        Console.Write("[");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i]);
+
+            
+            if (i < arr.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine("]");
+    }
+}
