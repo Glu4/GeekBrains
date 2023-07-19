07102af6a4cec8fd9ebe40dbc831cf55d9a688e4
Glu4 <avozhak@gmail.com>
2023-07-19 19:35:58 +0300

    success!

---
+++ Seminar4/Homework/Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+class Program
+{
+
+        static int CalculateDigit(int num)
+    {
+        int sum = 0;
+        while (num != 0)
+        {
+            int digit = num % 10; // Получаем последнюю цифру числа
+            sum += digit; // Добавляем цифру к сумме
+            num /= 10; // Убираем последнюю цифру числа
+        }
+        return sum;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Введите число:");
+        int number = int.Parse(Console.ReadLine());
+
+        int sum = CalculateDigit(number);
+        Console.WriteLine("Сумма цифр числа: " + sum);
+    }
+
+
+}
