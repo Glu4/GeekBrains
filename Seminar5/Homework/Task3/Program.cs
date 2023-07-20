12f7cabceb12f7631c97ce74db2eb4eb50d0915a
Glu4 <avozhak@gmail.com>
2023-07-20 12:11:34 +0300

    success

---
+++ Seminar5/Homework/Task3/Program.cs
@@ -0,0 +1,47 @@
+﻿//Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+
+{
+
+    static double FindMaxValue(double[] arr)
+    {
+        double max = arr[0];
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    
+    static double FindMinValue(double[] arr)
+    {
+        double min = arr[0];
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] < min)
+            {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+  
+        double[] numbers = { 3.22, 4.2, 1.15, 77.15, 65.2 };
+
+        
+        double maxNumber = FindMaxValue(numbers);
+        double minNumber = FindMinValue(numbers);
+
+       
+        double difference = maxNumber - minNumber;
+
+        
+        Console.WriteLine($"Разница между максимальным и минимальным элементами: {difference}");
+    }
+
