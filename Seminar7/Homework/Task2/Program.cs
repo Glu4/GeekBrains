57242f937ad55698a17ba676d6061d4a952f6d24
Glu4 <avozhak@gmail.com>
2023-07-27 17:51:05 +0300

    success

---
+++ Seminar7/Homework/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿    // Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+       
+       
+        int[,] array = new int[,]
+        {
+            { 1, 4, 7, 2 },
+            { 5, 9, 2, 3 },
+            { 8, 4, 2, 4 }
+        };
+
+        
+        Console.Write("Введите индекс строки: ");
+        int rowIndex = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите индекс столбца: ");
+        int colIndex = int.Parse(Console.ReadLine());
+
+        
+        if (rowIndex >= 0 && rowIndex < array.GetLength(0) && colIndex >= 0 && colIndex < array.GetLength(1))
+        {
+           
+            int value = array[rowIndex, colIndex];
+            Console.WriteLine($"Значение элемента: {value}");
+        }
+        else
+        {
+            Console.WriteLine("Неверные координаты элемента. Проверьте ввод.");
+        }
